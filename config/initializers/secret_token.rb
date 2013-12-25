bccffe12be4a44a9fc5d8370b18aedad04ceda84
Monty Lennie <montylennie@gmail.com>
2013-12-25 19:30:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearningDevise::Application.config.secret_key_base = 'f9715c1abc2c621e50490dbda15e042163a5c89544fce68e270ace3e30fc4a9157b6be00321b8549f0e90816a08ad70c332faf6be0aafd14f9f11c111fe44e97'
